ba22afe28cea66b7c5e966effbec38a149d8e676
Chedy soltani <chedysoltani0@gmail.com>
2025-10-27 14:51:17 +0100

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,8 @@ import { AlertController } from '@ionic/angular';
 export class AppComponent {
   constructor(private alertCtrl: AlertController) {}
 
-  async onStart() {
-    const alert = await this.alertCtrl.create({
-      header: 'Bonjour sur SIT E‑Learning',
-      message: "Apprenez n'importe où, n'importe quand.",
-      buttons: [{ text: 'Continuer', role: 'confirm' }],
-      cssClass: 'welcome-alert',
-      backdropDismiss: true,
-    });
-    await alert.present();
+  onStart() {
+    alert("Bonjour !");  // Très simple, fonctionne même dans QR preview
+    console.log("Bouton cliqué"); // Utile si tu testes via Chrome Remote Debug
   }
 }
